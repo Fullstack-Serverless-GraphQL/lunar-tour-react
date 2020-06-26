70d6ef05408063633eee2e0310b28989d96e3f3c
AmoDinho <u14284783@tuks.co.za>
2020-06-26 20:36:09 +0200

    querie is working

--- src/pages/ViewListing.js
+++ src/pages/ViewListing.js
@@ -1,7 +1,21 @@
 import React from "react";
+import RedBlockButton from "../components/buttons/RedBlockButton";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_A_LISTING } from "../graphql/Queries";
 
 const ViewListing = (props) => {
   console.log(props);
+
+  const { loading, data, error } = useQuery(GET_A_LISTING, {
+    variables: {
+      listingId: props.id,
+    },
+  });
+
+  if (loading) return <p className="text-red">loading</p>;
+  if (error) return <p className="text-red">{error}</p>;
+
+  console.log(data);
   return <></>;
 };
 
