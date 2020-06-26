a6c28fada5f6657ff6410e8af02a5e65c235b373
AmoDinho <u14284783@tuks.co.za>
2020-06-26 19:18:51 +0200

    Done with landing header

--- src/components/navs/LandingHeader.js
+++ src/components/navs/LandingHeader.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-const LandingHeader = () => (
-  <>
-    <div class=" text-center bg-transparent ">
-      <h1 class="text-white font-display text-5xl pt-44 s:text-4xl">
-        Book Vacations to Different Lunar Destinations
-      </h1>
-    </div>
-  </>
-);
+const LandingHeader = () => {
+  const imgURL =
+    "https://moon.nasa.gov/system/resources/detail_files/187_detail_as11-44-6551_orig.jpg";
+  return (
+    <>
+      <div
+        class=" text-center bg-transparent "
+        style={{
+          background: `url(${imgURL})no-repeat center center`,
+          backgroundSize: "cover",
+          height: "570px",
+          marginTop: "-85px",
+        }}
+      >
+        <h1 class="text-white font-display text-5xl pt-44 s:text-4xl">
+          Book Vacations to Different Lunar Destinations
+        </h1>
+      </div>
+    </>
+  );
+};
 
 export default LandingHeader;
