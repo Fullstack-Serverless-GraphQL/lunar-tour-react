48067a99a53a15dd7ed0c91fbdfbeb3e29f5424d
AmoDinho <u14284783@tuks.co.za>
2020-06-26 20:01:07 +0200

    create blueBlockButton

---
+++ src/components/buttons/BlueBlockButton.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { keyframes } from "styled-components";
+import "../../index.css";
+import loading from "../../assets/loading.svg";
+
+const propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+  disabled: PropTypes.bool,
+  large: PropTypes.bool,
+  long: PropTypes.bool,
+  isLoading: PropTypes.bool,
+};
+
+const rotate = keyframes`
+  from {
+	  transform: rotate(0deg);
+  }
+  to {
+	  transform: rotate(360deg);
+  }
+`;
+const ImageStyles = styled.img`
+  display: inline-block;
+  animation: ${rotate} 2s infinite linear;
+`;
+
+const DarkPinkButton = ({
+  className = "",
+  onClick,
+  text,
+  disabled = false,
+  isLoading,
+  large,
+  long,
+  ...props
+}) => {
+  return (
+    <button
+      /* eslint-disable no-template-curly-in-string*/
+      className={`${className} cursor-pointer bg-blue-lightest text-blue-darkest focus:outline-none font-display rounded-px px-16 py-2 hover:bg-transparent hover:border hover:border-blue-darkest`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <ImageStyles src={loading} alt="loading..." />
+    </button>
+  );
+};
+
+DarkPinkButton.propTypes = propTypes;
+export default DarkPinkButton;
