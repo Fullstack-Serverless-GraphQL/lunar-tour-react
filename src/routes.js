4c189f6fa80d4342e4b2cb576fe04af0f548e0bc
AmoDinho <u14284783@tuks.co.za>
2020-06-26 20:22:11 +0200

    imported the route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "@reach/router";
 import Home from "./pages/index";
+import ViewListing from "./pages/ViewListing";
 const Routes = ({ props }) => {
   return (
     <Router>
       <Home path="/" props={props} />
+      <ViewListing path="/listing/:id" props={props} />
     </Router>
   );
 };
