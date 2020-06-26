996a2a4e8a91c1ccc476e5c01cf1768563bba384
AmoDinho <u14284783@tuks.co.za>
2020-06-26 19:15:42 +0200

    added text

--- src/components/navs/LandingHeader.js
+++ src/components/navs/LandingHeader.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const LandingHeader = () => <></>;
+const LandingHeader = () => (
+  <>
+    <div class=" text-center bg-transparent ">
+      <h1 class="text-white font-display text-5xl pt-44 s:text-4xl">
+        Book Vacations to Different Lunar Destinations
+      </h1>
+    </div>
+  </>
+);
 
 export default LandingHeader;
