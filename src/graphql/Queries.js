17e369a1ebc8dda543269c2793eb4b6cec038478
AmoDinho <u14284783@tuks.co.za>
2020-06-26 20:29:17 +0200

    added schema doc

--- src/graphql/Queries.js
+++ src/graphql/Queries.js
@@ -18,3 +18,29 @@ export const GET_ALL_LISTINGS = gql`
     }
   }
 `;
+
+export const GET_A_LISTING = gql`
+  query GetAListing($listingId: String!) {
+    getAListing(listingId: $listingId) {
+      coverPhoto
+      listingId
+      listingName
+      listingType {
+        name
+      }
+      listingLocation
+      listingActivities {
+        name
+      }
+      listingDescription
+      price
+      numberOfDays
+      guide {
+        Bio
+        Name
+        Avatar
+      }
+      specialType
+    }
+  }
+`;
--- src/pages/ViewListing.js
+++ src/pages/ViewListing.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const ViewListing = () => {
+const ViewListing = (props) => {
+  console.log(props);
   return <></>;
 };
 
