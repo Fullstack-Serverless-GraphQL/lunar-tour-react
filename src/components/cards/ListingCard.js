b55c6944b14dcc43ea418d266daf5b02e2c38ef5
AmoDinho <u14284783@tuks.co.za>
2020-06-26 19:30:41 +0200

    done with basics of listing card

---
+++ src/components/cards/ListingCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ListingCard = ({
+  listingTitle,
+  listingLocation,
+  price,
+  rating,
+  coverPhoto,
+}) => (
+  <>
+    <div class="rounded-px w-card h-card shadow-lg-card cursor-pointer">
+      <img src={coverPhoto} class="w-full h-48 rounded-t-px" alt="cover" />
+      <div class="flex flex-col p-2">
+        <div class="flex flex-row mt-2 ">
+          <p class="font-display text-base">{listingTitle}</p>
+          <p class="font-display text-sm text-grey">{listingLocation}</p>
+        </div>
+
+        <p class="font-display text-lg mt-8">${price}</p>
+      </div>
+    </div>
+  </>
+);
+export default ListingCard;
