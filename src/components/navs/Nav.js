b4fc1e320ff57a907daabc435c7ad060c4505b39
AmoDinho <u14284783@tuks.co.za>
2020-06-26 19:06:48 +0200

    created nav component

---
+++ src/components/navs/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => (
+  <nav class="nav border-top flex items-center justify-between flex-wrap bg-transparent p-6 z-100">
+    <div class="flex items-center flex-no-shrink text">
+      <h1 class="font-display text-red text-2xl">
+        <a class="no-underline text-red" href="/">
+          Lunar
+        </a>
+      </h1>
+    </div>
+  </nav>
+);
+
+export default Nav;
