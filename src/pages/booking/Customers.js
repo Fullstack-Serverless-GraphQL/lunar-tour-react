f7af5c69b3af6508cb0d307caa2c683f11ea2057
AmoDinho <u14284783@tuks.co.za>
2020-06-29 20:12:03 +0200

    created customers tab

---
+++ src/pages/booking/Customers.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeadingOne from "../../components/typography/HeadingOne";
+import BodyOne from "../../components/typography/BodyOne";
+import Input from "../../components/inputs/Input";
+import RedBlockButton from "../../components/buttons/RedBlockButton";
+import RedOutlineButton from "../../components/buttons/RedOutlineButton";
+
+const Customers = (props) => {
+  return <></>;
+};
+
+export default Customers;
