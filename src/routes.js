8d9d1a33d9dcef8c58ce910dc12abd7fb704caee
AmoDinho <u14284783@tuks.co.za>
2020-06-29 19:45:22 +0200

    added it to the route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Router } from "@reach/router";
 import Home from "./pages/index";
 import ViewListing from "./pages/ViewListing";
+import BookingIndex from "./pages/booking";
 const Routes = ({ props }) => {
   return (
     <Router>
       <Home path="/" props={props} />
       <ViewListing path="/listing/:id" props={props} />
+      <BookingIndex path="/booking/:id" props={props} />
     </Router>
   );
 };
