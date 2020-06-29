dca864af4f6b8c3c5216ef9411d6327e48eb0a12
AmoDinho <u14284783@tuks.co.za>
2020-06-29 19:51:14 +0200

    added state

--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Tabs from "../../components/navs/Tabs";
 const BookingIndex = (props) => {
+  const [activeTab, setActiveTab] = useState(1);
   return (
     <>
-      <Tabs defaultActiveKey="1">
+      <Tabs defaultActiveKey={activeTab}>
         <Tabs.TabPane key="1">tab 1</Tabs.TabPane>
         <Tabs.TabPane key="2">tab 2</Tabs.TabPane>
       </Tabs>
