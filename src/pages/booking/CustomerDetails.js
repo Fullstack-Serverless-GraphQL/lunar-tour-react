522ed317295eda3106ba9d98051439836087b2af
AmoDinho <u14284783@tuks.co.za>
2020-06-29 20:31:46 +0200

    set up first form

--- src/pages/booking/CustomerDetails.js
+++ src/pages/booking/CustomerDetails.js
@@ -4,36 +4,39 @@ import BodyOne from "../../components/typography/BodyOne";
 import RedBlockButton from "../../components/buttons/RedBlockButton";
 import RedOutlineButton from "../../components/buttons/RedOutlineButton";
 import Input from "../../components/inputs/Input";
-
+import { Form } from "antd";
 const CustomerDetails = (props) => {
   console.log(props);
   return (
     <>
-      <div className="flex flex-col p-20 ">
-        <HeadingOne>Booking for Listing Name</HeadingOne>
-        <div className="mt-5">
-          <BodyOne>Booking date</BodyOne>
-          <Input placeholder="date" type="date" v-model="personal.date" />
-        </div>
+      <Form
+        fields={props.fields}
+        onFieldsChange={(changedFields, allFields) => {
+          props.setFields(allFields);
+        }}
+      >
+        <div className="flex flex-col p-20 ">
+          <HeadingOne>Booking for Listing Name</HeadingOne>
+          <div className="mt-5">
+            <BodyOne>Booking date</BodyOne>
+            <Input name="date" placeholder="date" type="date" />
+          </div>
 
-        <div className="mt-5">
-          <BodyOne> Email address </BodyOne>
-          <Input
-            placeholder="doku@corrisant.io"
-            type="email"
-            v-model="personal.email"
-          />
-        </div>
-        <div className="flex lg:flex-row mt-5 s:flex-col">
-          <RedBlockButton
-            text="Proceed"
-            className="mr-5 s:mb-5 lg:mb-0"
-            onClick={() => props.setActiveTab("2")}
-          />
+          <div className="mt-5">
+            <BodyOne> Email address </BodyOne>
+            <Input placeholder="doku@corrisant.io" name="email" type="email" />
+          </div>
+          <div className="flex lg:flex-row mt-5 s:flex-col">
+            <RedBlockButton
+              text="Proceed"
+              className="mr-5 s:mb-5 lg:mb-0"
+              onClick={() => props.setActiveTab("2")}
+            />
 
-          <RedOutlineButton text="Cancel" />
+            <RedOutlineButton text="Cancel" />
+          </div>
         </div>
-      </div>
+      </Form>
     </>
   );
 };
