9496d1adc275f076e1050be3e8e673e5f6982b37
AmoDinho <u14284783@tuks.co.za>
2020-06-29 20:29:13 +0200

    set up fields

--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -4,15 +4,20 @@ import CustomerDetails from "./CustomerDetails";
 import Customers from "./Customers";
 const BookingIndex = (props) => {
   const [activeTab, setActiveTab] = useState("1");
+  const [fields, setFields] = useState({});
   console.log(activeTab);
   return (
     <>
       <Tabs activeKey={activeTab}>
         <Tabs.TabPane key="1">
-          <CustomerDetails setActiveTab={setActiveTab} />
+          <CustomerDetails
+            setActiveTab={setActiveTab}
+            fields={fields}
+            setFields={setFields}
+          />
         </Tabs.TabPane>
         <Tabs.TabPane key="2">
-          <Customers setActiveTab={setActiveTab} />
+          <Customers setActiveTab={setActiveTab} fields={fields} />
         </Tabs.TabPane>
       </Tabs>
     </>
