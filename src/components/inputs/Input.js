5f9f182b0a48603c325318d66ab6ef31dbbde464
AmoDinho <u14284783@tuks.co.za>
2020-06-29 19:58:37 +0200

    created input component

---
+++ src/components/inputs/Input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+=
+
+const Input = ({
+  className,
+  value,
+  onChange,
+  ...props
+}) => {
+
+  return (
+    <input
+      className={`${className} 
+      bg-white 
+      rounded-px 
+      py-3
+      px-16 
+      border
+      border-grey-light 
+      focus:outline-none 
+      focus:bg-white"`}
+      value={value}
+      onChange={onChange}
+      {...props}
+    />
+  )
+
+}
+
+const propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+}
+Input.propTypes = propTypes
+export default Input
