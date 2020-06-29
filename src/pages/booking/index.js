a79ed9511fff2bc560db96a5338facd00fe15570
AmoDinho <u14284783@tuks.co.za>
2020-06-29 19:50:14 +0200

    tabs are working

--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Tabs from "../../components/navs/Tabs";
 const BookingIndex = (props) => {
-  return <></>;
+  return (
+    <>
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane key="1">tab 1</Tabs.TabPane>
+        <Tabs.TabPane key="2">tab 2</Tabs.TabPane>
+      </Tabs>
+    </>
+  );
 };
 
 export default BookingIndex;
