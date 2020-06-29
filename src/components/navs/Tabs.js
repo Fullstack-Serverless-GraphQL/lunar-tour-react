0e6f09a16ea69cdf240bae7dbec7031be88e5de4
AmoDinho <u14284783@tuks.co.za>
2020-06-29 19:42:57 +0200

    added css overwrite

--- src/components/navs/Tabs.js
+++ src/components/navs/Tabs.js
@@ -1,6 +1,9 @@
-import React from "react";
 import styled from "styled-components";
 import { Tabs } from "antd";
-const TabStyles = styled(Tabs)``;
+const TabStyles = styled(Tabs)`
+  .ant-tabs-bar.ant-tabs-top-bar {
+    display: none;
+  }
+`;
 
 export default TabStyles;
