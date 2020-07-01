c69ee1f9196c5f50c68303cd13f50d8e903b1d0d
AmoDinho <u14284783@tuks.co.za>
2020-07-01 19:35:08 +0200

    done with resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,5 @@
 import gql from "graphql-tag";
-
+import { GET_FORM_DATA } from "./graphql/Queries";
 export const typeDefs = gql`
   extend type Query {
     formData: FormData
@@ -35,4 +35,25 @@ export const typeDefs = gql`
     ): FormData
   }
 `;
-export const resolvers = {};
+export const resolvers = {
+  Mutation: {
+    updateFormData: (_, date, email, customers, { cache }) => {
+      const queryResult = cache.readQuery({ query: GET_FORM_DATA });
+
+      if (queryResult) {
+        const { formData } = queryResult;
+        const data = {
+          formData: {
+            date: date,
+            email: email,
+            customers: customers,
+            ...formData,
+          },
+        };
+        cache.writeQuery({ query: GET_FORM_DATA, data });
+        return data.formData;
+      }
+      return [];
+    },
+  },
+};
