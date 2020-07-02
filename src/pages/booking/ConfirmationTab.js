0740a141d98bc16e10a6e8d0ff151ecb1e680560
AmoDinho <u14284783@tuks.co.za>
2020-07-02 21:34:33 +0200

    last page is set up

--- src/pages/booking/ConfirmationTab.js
+++ src/pages/booking/ConfirmationTab.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import HeadingOne from "../../components/typography/HeadingOne";
+import BodyOne from "../../components/typography/BodyOne";
+import RedBlockButton from "../../components/buttons/RedBlockButton";
+import vector from "../../assets/Vector.svg";
+import ticket from "../../assets/confirmation_number.svg";
 const ConfirmationTab = (props) => {
-  return <></>;
+  return (
+    <>
+      <div class="flex flex-col p-20 ">
+        <HeadingOne>Thanks for booking with us</HeadingOne>
+        <img src={vector} alt="vector" class="mt-5" />
+        <BodyOne className="mt-5">
+          Your link to your ticket is in the mail. Keep it safe and we will see
+          you soon on{" "}
+        </BodyOne>
+        <div>
+          <img src={ticket} alt="ticket" />
+          <a class="text-yellow font-display" href="bookingData.chargeReciept">
+            Tickets
+          </a>
+        </div>
+        <div class="flex flex-row ml-32 mt-20">
+          <RedBlockButton text="Book more" className="mr-5" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ConfirmationTab;
