cb6951778b94db6146191a28252733daf532a951
AmoDinho <u14284783@tuks.co.za>
2020-07-02 21:26:12 +0200

    buttons  are added

--- src/pages/booking/Checkout.js
+++ src/pages/booking/Checkout.js
@@ -6,6 +6,10 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
+import HeadingOne from "../../components/typography/HeadingOne";
+import BodyOne from "../../components/typography/BodyOne";
+import RedBlockButton from "../../components/buttons/RedBlockButton";
+import RedOutlineButton from "../../components/buttons/RedOutlineButton";
 
 const StripeElements = () => {
   const stripe = useStripe();
@@ -21,7 +25,20 @@ const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 const Checkout = (props) => {
   return (
     <Elements stripe={stripePromise}>
+      <HeadingOne>Your trip total</HeadingOne>
+      <BodyOne>
+        Test using this credit card: 4242 4242 4242 4242, and enter any 5 digits
+        for the zip code
+      </BodyOne>
       <StripeElements />
+      <div class="flex flex-row mt-20">
+        <RedBlockButton
+          text="Proceed"
+          className="mr-5 s:mb-5  lg:mb-0"
+          onClick={() => props.setActiveTab("4")}
+        />
+        <RedOutlineButton text="Back" onClick={() => props.setActiveTab("2")} />
+      </div>
     </Elements>
   );
 };
