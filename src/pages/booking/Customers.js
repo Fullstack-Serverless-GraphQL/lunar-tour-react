a5994b92410313528b338900536b6fd00b9e045e
AmoDinho <u14284783@tuks.co.za>
2020-07-02 20:29:47 +0200

    added tthe update part of the mutation

--- src/pages/booking/Customers.js
+++ src/pages/booking/Customers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { UPDATE_FORM_DATA } from "../../graphql/Mutations";
 import { useMutation } from "@apollo/react-hooks";
 import BodyOne from "../../components/typography/BodyOne";
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,20 +1,38 @@
-import gql from "graphql-tag";
+import _ from "lodash";
 import { GET_FORM_DATA } from "./graphql/Queries";
 
 export const resolvers = {
   Mutation: {
-    updateFormData: (_, args, context, info) => {
+    updateFormData: (parent, args, context, info) => {
       console.log("rrr", args);
       const queryResult = context.cache.readQuery({ query: GET_FORM_DATA });
       const { formData } = queryResult;
       if (queryResult) {
-        const data = {
-          formData: formData.hasOwnProperty(""),
-        };
+        if (!_.isUndefined(formData.date) && !_.isUndefined(formData.email)) {
+          const data = {
+            formData: {
+              date: formData.date,
+              email: formData.email,
+              customer: args.customers,
+              __typename: formData["__typename"],
+            },
+          };
 
-        context.cache.writeQuery({ query: GET_FORM_DATA, data });
+          context.cache.writeQuery({ query: GET_FORM_DATA, data });
+          return data.formData;
+        } else {
+          const data = {
+            formData: {
+              date: args.date,
+              email: args.email,
+              customer: args.customers,
+              __typename: formData["__typename"],
+            },
+          };
 
-        return data.formData;
+          context.cache.writeQuery({ query: GET_FORM_DATA, data });
+          return data.formData;
+        }
       }
       return [];
     },
