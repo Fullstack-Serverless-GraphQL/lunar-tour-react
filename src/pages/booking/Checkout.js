47b0ef6fb949ea17dad04dce9014fe232c055ebf
AmoDinho <u14284783@tuks.co.za>
2020-07-02 21:11:37 +0200

    created checkout form

---
+++ src/pages/booking/Checkout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+const Checkout = () => {
+  return <></>;
+};
+
+export default Checkout;
