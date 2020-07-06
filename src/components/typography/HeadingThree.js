793be4fe53e59bc3b8a872e4caf2969590206738
AmoDinho <u14284783@tuks.co.za>
2020-07-06 20:16:59 +0200

    created HeadingThree

---
+++ src/components/typography/HeadingThree.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+const HeadingThree = ({ className, children }) => {
+  return (
+    <h1 className={`${className} text-2xl text-black text-bold font-display`}>
+      {children}
+    </h1>
+  );
+};
+
+HeadingThree.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+HeadingThree.defaultProps = {
+  className: "",
+};
+export default HeadingThree;
