fe3b9c927653b3aff70e7fd069b98cdee48840a5
AmoDinho <u14284783@tuks.co.za>
2020-07-06 20:15:56 +0200

    created HeadingTwo

---
+++ src/components/typography/HeadingTwo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+const HeadingTwo = ({ className, children }) => {
+  return (
+    <h1 className={`${className} text-xl text-black text-bold font-display`}>
+      {children}
+    </h1>
+  );
+};
+
+HeadingTwo.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+HeadingTwo.defaultProps = {
+  className: "",
+};
+export default HeadingTwo;
