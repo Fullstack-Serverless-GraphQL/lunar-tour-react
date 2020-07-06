ff0e8e744fcb582780e439d6c2971224bf7d5ed8
AmoDinho <u14284783@tuks.co.za>
2020-07-06 21:15:27 +0200

    fixed class warning

--- src/components/navs/Nav.js
+++ src/components/navs/Nav.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Nav = () => (
-  <nav class="nav border-top flex items-center justify-between flex-wrap bg-transparent p-55 z-100">
-    <div class="flex items-center flex-no-shrink text">
-      <h1 class="font-display text-red text-2xl">
-        <a class="no-underline text-red" href="/">
+  <nav className="nav border-top flex items-center justify-between flex-wrap bg-transparent p-55 z-100">
+    <div className="flex items-center flex-no-shrink text">
+      <h1 className="font-display text-red text-2xl">
+        <a className="no-underline text-red" href="/">
           Lunar
         </a>
       </h1>
