c52e6d805648e21f4e81950e008dec7460ffedde
AmoDinho <u14284783@tuks.co.za>
2020-07-06 19:57:17 +0200

    fixed the padding of the nav bar

--- src/components/navs/Nav.js
+++ src/components/navs/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Nav = () => (
-  <nav class="nav border-top flex items-center justify-between flex-wrap bg-transparent p-6 z-100">
+  <nav class="nav border-top flex items-center justify-between flex-wrap bg-transparent p-55 z-100">
     <div class="flex items-center flex-no-shrink text">
       <h1 class="font-display text-red text-2xl">
         <a class="no-underline text-red" href="/">
