36c553e4315ad839dcf1885dc220c11ebcdd27a5
AmoDinho <u14284783@tuks.co.za>
2020-07-07 20:15:20 +0200

    fixedd checkout screen

--- src/pages/booking/Checkout.js
+++ src/pages/booking/Checkout.js
@@ -25,19 +25,24 @@ const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 const Checkout = (props) => {
   return (
     <Elements stripe={stripePromise}>
-      <HeadingOne>Your trip total</HeadingOne>
-      <BodyOne>
-        Test using this credit card: 4242 4242 4242 4242, and enter any 5 digits
-        for the zip code
-      </BodyOne>
-      <StripeElements />
-      <div class="flex flex-row mt-20">
-        <RedBlockButton
-          text="Proceed"
-          className="mr-5 s:mb-5  lg:mb-0"
-          onClick={() => props.setActiveTab("4")}
-        />
-        <RedOutlineButton text="Back" onClick={() => props.setActiveTab("2")} />
+      <div className="flex flex-col">
+        <HeadingOne>Your trip total</HeadingOne>
+        <BodyOne>
+          Test using this credit card: 4242 4242 4242 4242, and enter any 5
+          digits for the zip code
+        </BodyOne>
+        <StripeElements />
+        <div class="flex flex-row mt-20">
+          <RedBlockButton
+            text="Proceed"
+            className="mr-5 s:mb-5  lg:mb-0"
+            onClick={() => props.setActiveTab("4")}
+          />
+          <RedOutlineButton
+            text="Back"
+            onClick={() => props.setActiveTab("2")}
+          />
+        </div>
       </div>
     </Elements>
   );
