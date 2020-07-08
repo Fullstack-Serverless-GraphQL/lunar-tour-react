602d5ef3f9efff2adeb03325e758fa5a7ad72824
AmoDinho <u14284783@tuks.co.za>
2020-07-08 20:43:07 +0200

    crreated a MAKE_A_BOOKING schema doc

--- src/graphql/Mutations.js
+++ src/graphql/Mutations.js
@@ -9,3 +9,34 @@ export const UPDATE_FORM_DATA = gql`
     updateFormData(date: $date, email: $email, customer: $customer) @client
   }
 `;
+
+export const MAKE_A_BOOKING = gql`
+  mutation MAKE_A_BOOKING(
+    $customerEmail: String
+    $bookingDate: String
+    $listingId: String
+    $customers: [CustomerInput]
+  ) {
+    makeABooking(
+      customerEmail: $customerEmail
+      bookingDate: $bookingDate
+      listingId: $listingId
+      customers: $customers
+    ) {
+      bookingId
+      listingId
+      bookingDate
+      size
+      bookingTotal
+      customerEmail
+      customers {
+        name
+        surname
+        country
+        passportNumber
+        physioScore
+      }
+      chargeReciept
+    }
+  }
+`;
